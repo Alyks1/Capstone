24cc829dbe968118fe4570af63b398c2d395dfff
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2023-02-20 18:02:40 +0100

    (refactor) refactors date extraction regex does not work
    Signed-off-by: Alexander Gemmingen <alexander.gemmingen@gmx.de>

--- createData.ts
+++ createData.ts
@@ -7,21 +7,38 @@ export async function CreateDataSetFromPost(posts: Post[], page: Page) {
     //There must be a better way to do this but here we are
     for (let post of posts) {
         //TODO: Fix this
-        const filename = createFilename(post.text);
+        const filename = extractDates(post.text);
+        //Use fetch to download img
         await page.goto(post.imgSrc);
         await page.pdf({ path: `./Images/${filename}.pdf` })
     };
 }
 
-function createFilename(text: string) {
-    //Maybe remove all , and . before this.
-    const sanatizedText = text.replace(",", "").replace(".", "");
-    var regexp = new RegExp('((?<!\\\[|\\\()[0-9]+(BCE|AD|BC|CE)?(-[0-9]+)?( )?(BCE|AD|BC|CE)?(S| )(?!X|×))|([0-9]+(ND|ST|RD|TH)( )*[A-Z]*( )(BCE|AD|BC|CE)?)', 'i');
+function extractDates(text: string) {
+    //Remove all , and . before this.
+    const sanatizedText = text.replace(/[.,]/g, "");
 
-    //This matches year numbers like 1532 and 24-56AD (?<!\[|\()[0-9]+(BCE|AD|BC|CE)?(-[0-9]+)?( )?(BCE|AD|BC|CE)?(S| )(?!X|×)
-    //This matches time frames like 7th century BC [0-9]+(ND|ST|RD|TH)( )*[A-Z]*( )?(BCE|AD|BC|CE)? TODO: Fix this regex to allow "4th century bc until 5th century AD"
-    const results = regexp.exec(sanatizedText);
-    results.map(x => console.log(x));
+    //This matches year numbers like 1532 and 24-56AD
+    const yearRegexp = /(?<![[(])[0-9]+(BCE|AD|BC|CE)?(-[0-9]+)?( )?(BCE|AD|BC|CE)?(S| |-)(?!X|×)/ig;
+    //This matches time frames like 7th century BC
+    const rangeRegexp = /[0-9]+(ST|ND|RD|TH)( )*[A-Z]*( )?[0-9]*(ST|ND|RD|TH)*( )?(\bcentury\b)+( )?(BCE|AD|BC|CE)?/ig;
 
-    return results[0];
+    console.log(sanatizedText);
+    let results = [];
+
+    if (sanatizedText.matchAll(yearRegexp)) {
+        results = [...sanatizedText.matchAll(yearRegexp)]
+    }
+    else if (sanatizedText.matchAll(rangeRegexp)) {
+        results = [...sanatizedText.matchAll(rangeRegexp)]
+    }
+
+    // if (yearRegexp.test(sanatizedText)) {
+    //     results = yearRegexp.exec(sanatizedText);
+    // } else if (rangeRegexp.test(sanatizedText)) {
+    //     results = rangeRegexp.exec(sanatizedText);
+    // }
+
+    console.log(results)
+    return results[0][0];
 }
