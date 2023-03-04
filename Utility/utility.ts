fcef147d72eae2e2230f5280a10180e80bbe9048
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2023-03-04 14:34:32 +0100

    (chore) Removed depreciated code
    Signed-off-by: Alexander Gemmingen <alexander.gemmingen@gmx.de>

--- Utility/utility.ts
+++ Utility/utility.ts
@@ -1,17 +1,4 @@
 export class Utility {
-	static difference(a: number, b: number) {
-		return Math.abs(a - b);
-	}
-
-	static getDifferences(data: string[]): number[] {
-		let result: number[] = [];
-		for (let i = 0; i < data.length; i++) {
-			if (i + 1 < data.length)
-				result.push(this.difference(+data[i], +data[i + 1]));
-		}
-		return result;
-	}
-
 	static sleep(ms: number) {
 		return new Promise((resolve) => setTimeout(resolve, ms));
 	}
