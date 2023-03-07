15a590800298bdcd3dba70580d6abb7cd4a70641
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2023-03-07 11:29:32 +0100

    (feat) adds Websockets to start scraping
    Signed-off-by: Alexander Gemmingen <alexander.gemmingen@gmx.de>

---
+++ frontend/script.js
@@ -0,0 +1,8 @@
+import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
+
+const startScraperButton = document.getElementById("start-button");
+const socket = io("http://localhost:3000");
+startScraperButton.addEventListener("click", () => {
+	console.log("Starting Scraper");
+	socket.emit("start");
+});
