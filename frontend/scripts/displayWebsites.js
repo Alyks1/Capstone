b21ed3481c90c0009cd3450201c6fd394c6f0b67
Alyks1 <alexander.gemmingen@gmx.de>
2023-03-17 15:40:28 +0100

    (refactor) refactors displayWebsites creating list
    Signed-off-by: Alyks1 <alexander.gemmingen@gmx.de>

--- frontend/scripts/displayWebsites.js
+++ frontend/scripts/displayWebsites.js
@@ -9,29 +9,49 @@ socket.emit("getWebsites");
 
 socket.on("websites", (websites) => {
     console.log(`received websites`);
-    websites.forEach((website, i) => {
-        console.log(JSON.stringify(website));
-        const url = cleanUrl(website.url);
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-        const button = document.createElement("button");
-        button.textContent = "Deactivate";
-        button.addEventListener("click", () => {
-            socket.emit("deactivateWebsite", i);
-            window.location.href = "displayWebsiteList.html";
-        });
-        a.href = `updateWebsite.html?id=${i}`;
-        a.textContent = url;
-        li.appendChild(a);
-        const p = document.createElement("p");
-        p.textContent = `NrOfPages: ${website.nrOfPages} - Weight: ${website.weight}`;
-        li.appendChild(p);
-        li.appendChild(button);
-        list.appendChild(li);
-    });
+    createList(websites);
 })
 
 returnButton.addEventListener("click", () => {
     window.location.href = "index.html";
 });
 
+function createList(websites) {
+    websites.forEach((website, i) => {
+        const url = cleanUrl(website.url);
+
+        const li = document.createElement("li");
+
+        const a = createA(url, i);
+        const p = createP(website);
+        const button = createDeactivateButton();
+
+        li.appendChild(a);
+        li.appendChild(p);
+        li.appendChild(button);
+        list.appendChild(li);
+    });
+}
+
+function createDeactivateButton() {
+    const button = document.createElement("button");
+    button.textContent = "Deactivate";
+    button.addEventListener("click", () => {
+        socket.emit("deactivateWebsite", i);
+        window.location.href = "displayWebsiteList.html";
+    });
+    return button
+}
+
+function createA(url, i) {
+    const a = document.createElement("a");
+    a.href = `updateWebsite.html?id=${i}`;
+    a.textContent = url;
+    return a;
+}
+
+function createP(website) {
+    const p = document.createElement("p");
+    p.textContent = `NrOfPages: ${website.nrOfPages} - Weight: ${website.weight}`;
+    return p;
+}
