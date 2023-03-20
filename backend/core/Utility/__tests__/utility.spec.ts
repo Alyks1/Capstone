b2680016196f9f80484b461c5009a99d37b57c6c
Alyks1 <alexander.gemmingen@gmx.de>
2023-03-20 13:38:08 +0100

    (test) adds tests for utility methods
    Signed-off-by: Alyks1 <alexander.gemmingen@gmx.de>

---
+++ backend/core/Utility/__tests__/utility.spec.ts
@@ -0,0 +1,41 @@
+import {Utility} from "../utility";
+
+/*
+    .replace(/[–/~]/g, "-")
+    .replace(/[.,;]/g, "")
+    .replace(/([\(\[])([0-9])*([x× ])+[0-9]*([\)\]])/g, "") //Remove img resolution eg (1080x960)
+    .replace(/([\(\[\)\]])/g, "")
+    .replace(/(\bBCE\b)/gi, "BC") //Replace BCE with BC
+    .replace(/(\bCE\b)/gi, "AD") //Replace CE with AD
+    .replace(/(st|nd|rd|th)/gi, "") 
+    .toLowerCase();
+*/
+const testCases = [
+    {input: "Clean chars like – and ~", expect: "clean chars like - a -"},
+    {input: "Remove punctuation like . , ;", expect: "remove punctuation like   "},
+    {input: "Remove img resolution like (1080x960),[1440 x 1080],(1080 x 960),[240×12]", expect: "remove img resolution like "},
+    {input: "Remove brackets like () or []", expect: "remove brackets like  or "},
+    {input: "Convert BCE or bce", expect: "convert bc or bc"},
+    {input: "Convert CE or ce", expect: "convert ad or ad"},
+    {input: "Clean numbers like 1st, 2nd, 3rd, 4th etc", expect: "clean numbers like 1 2 3 4 etc"},
+    {input: "Convert tO Lowercase", expect: "convert to lowercase"},
+]
+describe("sanatizeText", () => {
+    it("should clean the text", () => {
+        testCases.forEach((testCase) => {
+            expect(Utility.sanatizeText(testCase.input)).toBe(testCase.expect);
+        });
+    });
+});
+
+describe("isNumber", () => {
+    it("should return true for a number", () => {
+        expect(Utility.isNumber("1")).toBe(true);
+    });
+    it("should return false for a string", () => {
+        expect(Utility.isNumber("a")).toBe(false);
+    });
+    it("should return false for a string that contains a number", () => {
+        expect(Utility.isNumber("5th")).toBe(false);
+    });
+});
