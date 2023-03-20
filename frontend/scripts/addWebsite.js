51d6882f0f90cd8007390a52328782940f56bdb3
Alyks1 <alexander.gemmingen@gmx.de>
2023-03-20 14:24:55 +0100

    (fix) fixed more hacks
    Signed-off-by: Alyks1 <alexander.gemmingen@gmx.de>

--- frontend/scripts/addWebsite.js
+++ frontend/scripts/addWebsite.js
@@ -1,18 +1,25 @@
 import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
 import { getSocketURL } from "./utility.js";
 
-const inputs = document.getElementById("form").getElementsByTagName("input");
+const form = document.getElementById("form");
 const submitButton = document.getElementById("submit");
 const returnButton = document.getElementById("returnButton");
 const socket = io(getSocketURL());
 
 submitButton.addEventListener("click", () => {
 	console.log("Adding website");
+
+	const inputs = form.elements;
+	const url = inputs.namedItem("websiteUrl").value ?? "";
+	const weight = inputs.namedItem("weight").value ?? "";
+	const nrOfPages = inputs.namedItem("nrOfPages").value ?? "";
+
+	if (!inputsOK(inputs)) return;
 	socket.emit("addWebsite",
 		{
-			url: inputs.namedItem("websiteUrl").value,
-			weight: Number(inputs.namedItem("weight").value),
-			nrOfPages: Number(inputs.namedItem("nrOfPages").value),
+			url: url,
+			weight: Number(weight),
+			nrOfPages: Number(nrOfPages),
 		}
 	);
 });
@@ -24,3 +31,21 @@ socket.on("log", (msg) => {
 returnButton.addEventListener("click", () => {
 	window.location.href = "index.html";
 });
+
+function inputsOK(inputs) {
+	for (let i = 0; i < inputs.length; i++) {
+		if (inputs[i].value == "") {
+			alert("Please fill out all fields");
+			return false;
+		}
+		if (inputs["weight"].value > 1 || inputs["weight"].value < 0) {
+			alert("Min weight is 0 and max is 1");
+			return false;
+		}
+		if (inputs["nrOfPages"].value > 10 || inputs["nrOfPages"].value < 0) {
+			alert("min nr of pages is 0 and max is 10");
+			return false;
+		}
+	}
+	return true;
+}
