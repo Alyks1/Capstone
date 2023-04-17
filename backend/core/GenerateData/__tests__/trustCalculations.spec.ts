b1b0df693005de13ed3ba0b482c44a23a80743db
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2023-04-17 22:27:51 +0200

    (test) adds test for trustCalc
    Signed-off-by: Alexander Gemmingen <alexander.gemmingen@gmx.de>

--- backend/core/GenerateData/__tests__/trustCalculations.spec.ts
+++ backend/core/GenerateData/__tests__/trustCalculations.spec.ts
@@ -1,11 +1,9 @@
-import { calcTrust, trustCalcOptions } from "../trustCalculations";
-
-const options = { ...trustCalcOptions };
+import { TrustCalcOptions, calcTrust } from "../trustCalculations";
 
 describe("calcTrust", () => {
 	const data = [
 		{
-			date: "5",
+			date: "6",
 			trust: 1,
 			pos: 0,
 		},
@@ -20,19 +18,111 @@ describe("calcTrust", () => {
 			pos: 0,
 		},
 		{
-			date: "50",
+			date: "66",
 			trust: 1,
 			pos: 0,
 		},
 	];
 	it("should calc with all options off", () => {
-		options.between0and10 = false;
-		options.differentNr = false;
-		options.multipleOf10and5 = false;
-		options.notBetween0and100 = false;
-		options.reduceTrust = 0;
+		const options: TrustCalcOptions = {
+			notBetween0and10: false,
+			differentNr: false,
+			notMultipleOf10and5: false,
+			notBetween0and100: false,
+			reduceTrust: 0,
+		};
 		const result = calcTrust(data, JSON.stringify(options));
 		expect(result[0].trust).toEqual(1);
+		expect(result[1].trust).toEqual(1);
+		expect(result[2].trust).toEqual(1);
+		expect(result[3].trust).toEqual(1);
+		data.forEach((x) => {
+			x.trust = 1;
+		});
+	});
+	it("should increase trust for all but index 0", () => {
+		const options: TrustCalcOptions = {
+			notBetween0and10: true,
+			differentNr: false,
+			notMultipleOf10and5: false,
+			notBetween0and100: false,
+			reduceTrust: 0,
+		};
+		const result = calcTrust(data, JSON.stringify(options));
 		expect(result[0].trust).toEqual(1);
+		expect(result[1].trust).toEqual(2);
+		expect(result[2].trust).toEqual(2);
+		expect(result[3].trust).toEqual(2);
+		data.forEach((x) => {
+			x.trust = 1;
+		});
+	});
+	it("should increase trust for only index 0 and 1", () => {
+		const options: TrustCalcOptions = {
+			notBetween0and10: true,
+			differentNr: true,
+			notMultipleOf10and5: false,
+			notBetween0and100: false,
+			reduceTrust: 0,
+		};
+		const result = calcTrust(data, JSON.stringify(options));
+		expect(result[0].trust).toEqual(2);
+		expect(result[1].trust).toEqual(3);
+		expect(result[2].trust).toEqual(2);
+		expect(result[3].trust).toEqual(2);
+		data.forEach((x) => {
+			x.trust = 1;
+		});
+	});
+	it("should increase trust for all but index 2", () => {
+		const options: TrustCalcOptions = {
+			notBetween0and10: true,
+			differentNr: true,
+			notMultipleOf10and5: true,
+			notBetween0and100: false,
+			reduceTrust: 0,
+		};
+		const result = calcTrust(data, JSON.stringify(options));
+		expect(result[0].trust).toEqual(3);
+		expect(result[1].trust).toEqual(4);
+		expect(result[2].trust).toEqual(2);
+		expect(result[3].trust).toEqual(3);
+		data.forEach((x) => {
+			x.trust = 1;
+		});
+	});
+	it("should increase trust for all but index 0 and 3", () => {
+		const options: TrustCalcOptions = {
+			notBetween0and10: true,
+			differentNr: true,
+			notMultipleOf10and5: true,
+			notBetween0and100: true,
+			reduceTrust: 0,
+		};
+		const result = calcTrust(data, JSON.stringify(options));
+		expect(result[0].trust).toEqual(3);
+		expect(result[1].trust).toEqual(5);
+		expect(result[2].trust).toEqual(3);
+		expect(result[3].trust).toEqual(3);
+		data.forEach((x) => {
+			x.trust = 1;
+		});
+	});
+	it("should decrease trust for all by 2", () => {
+		const options: TrustCalcOptions = {
+			notBetween0and10: true,
+			differentNr: true,
+			notMultipleOf10and5: true,
+			notBetween0and100: true,
+			reduceTrust: 2,
+		};
+		const result = calcTrust(data, JSON.stringify(options));
+		expect(result[0].trust).toEqual(1);
+		expect(result[1].trust).toEqual(3);
+		expect(result[2].trust).toEqual(1);
+		expect(result[3].trust).toEqual(1);
+		data.forEach((x) => {
+			x.trust = 1;
+		});
 	});
 });
--- backend/core/GenerateData/trustCalculations.ts
+++ backend/core/GenerateData/trustCalculations.ts
@@ -5,17 +5,17 @@ import { Logger } from "../Utility/logging";
 export interface TrustCalcOptions {
 	notBetween0and100: boolean;
 	differentNr: boolean;
-	multipleOf10and5: boolean;
-	between0and10: boolean;
+	notMultipleOf10and5: boolean;
+	notBetween0and10: boolean;
 	reduceTrust: number;
 }
 
 // rome-ignore lint/style/useConst: these should be updated
-export let trustCalcOptions: TrustCalcOptions = {
+let trustCalcOptions: TrustCalcOptions = {
 	notBetween0and100: true,
 	differentNr: false,
-	multipleOf10and5: true,
-	between0and10: true,
+	notMultipleOf10and5: true,
+	notBetween0and10: true,
 	reduceTrust: 1,
 };
 
@@ -28,21 +28,21 @@ export function calcTrust(
 	data: WorkingData[],
 	trustCalcOptions = getTrustCalcOptions(),
 ) {
-	const options = JSON.parse(trustCalcOptions);
+	const options: TrustCalcOptions = JSON.parse(trustCalcOptions);
 	data.forEach((x) => {
 		Logger.trace(`Before: ${x.date} : ${x.trust}`);
-		//If the date is not between 0 and 100
-		if (+x.date < 0 || +x.date > 101)
-			x.trust = adjustTrust(x.trust, 1, options.notBetween0and100);
-		//If many different numbers, more precision
+		//if the date is not between 1 and 10, increase trust
+		if (+x.date < 0 || +x.date > 11)
+			x.trust = adjustTrust(x.trust, 1, options.notBetween0and10);
+		//If many different numbers, more precision, increase trust
 		if (new Set([...x.date]).size === x.date.length)
 			x.trust = adjustTrust(x.trust, 1, options.differentNr);
-		//if the date is not a multiple of 5, more precision
+		//if the date is not a multiple of 5, more precision, increase trust
 		if (+x.date % 5 !== 0)
-			x.trust = adjustTrust(x.trust, 1, options.multipleOf10and5);
-		//if the date is between 1 and 10, less likely to be a year
-		if (+x.date < 0 || +x.date > 11)
-			x.trust = adjustTrust(x.trust, 1, options.between0and10);
+			x.trust = adjustTrust(x.trust, 1, options.notMultipleOf10and5);
+		//If the date is not between 0 and 100 increase trust
+		if (+x.date < 0 || +x.date > 101)
+			x.trust = adjustTrust(x.trust, 1, options.notBetween0and100);
 		//reduce trust by one to stop trust inflation
 		Logger.debug(`Reduce trust: ${options.reduceTrust}`);
 		x.trust = adjustTrust(x.trust, options.reduceTrust * -1, true);
@@ -60,8 +60,8 @@ export async function setTrustCalcOptions(activations) {
 	const jsonData: TrustCalcOptions = {
 		notBetween0and100: activations.notBetween0and100,
 		differentNr: activations.differentNr,
-		multipleOf10and5: activations.multipleOf10and5,
-		between0and10: activations.between0and10,
+		notMultipleOf10and5: activations.notMultipleOf10and5,
+		notBetween0and10: activations.notBetween0and10,
 		reduceTrust: activations.reduceTrust,
 	};
 	const writeData = JSON.stringify(jsonData);
@@ -73,8 +73,8 @@ export function getTrustCalcOptions() {
 	const data: TrustCalcOptions = JSON.parse(buffer.toString());
 	trustCalcOptions.notBetween0and100 = data.notBetween0and100;
 	trustCalcOptions.differentNr = data.differentNr;
-	trustCalcOptions.multipleOf10and5 = data.multipleOf10and5;
-	trustCalcOptions.between0and10 = data.between0and10;
+	trustCalcOptions.notMultipleOf10and5 = data.notMultipleOf10and5;
+	trustCalcOptions.notBetween0and10 = data.notBetween0and10;
 	trustCalcOptions.reduceTrust = Number(data.reduceTrust);
 
 	return JSON.stringify(data);
