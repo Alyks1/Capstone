13010ad76f137da9973451985cce4090eef7e3aa
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2023-04-17 22:30:56 +0200

    (refactor) refactors trustCalc test
    Signed-off-by: Alexander Gemmingen <alexander.gemmingen@gmx.de>

--- backend/core/GenerateData/__tests__/trustCalculations.spec.ts
+++ backend/core/GenerateData/__tests__/trustCalculations.spec.ts
@@ -1,5 +1,13 @@
 import { TrustCalcOptions, calcTrust } from "../trustCalculations";
 
+const options: TrustCalcOptions = {
+	notBetween0and10: false,
+	differentNr: false,
+	notMultipleOf10and5: false,
+	notBetween0and100: false,
+	reduceTrust: 0,
+};
+
 describe("calcTrust", () => {
 	const data = [
 		{
@@ -24,105 +32,65 @@ describe("calcTrust", () => {
 		},
 	];
 	it("should calc with all options off", () => {
-		const options: TrustCalcOptions = {
-			notBetween0and10: false,
-			differentNr: false,
-			notMultipleOf10and5: false,
-			notBetween0and100: false,
-			reduceTrust: 0,
-		};
 		const result = calcTrust(data, JSON.stringify(options));
 		expect(result[0].trust).toEqual(1);
 		expect(result[1].trust).toEqual(1);
 		expect(result[2].trust).toEqual(1);
 		expect(result[3].trust).toEqual(1);
+	});
+	it("should increase trust for all but index 0", () => {
 		data.forEach((x) => {
 			x.trust = 1;
 		});
-	});
-	it("should increase trust for all but index 0", () => {
-		const options: TrustCalcOptions = {
-			notBetween0and10: true,
-			differentNr: false,
-			notMultipleOf10and5: false,
-			notBetween0and100: false,
-			reduceTrust: 0,
-		};
+		options.notBetween0and10 = true;
 		const result = calcTrust(data, JSON.stringify(options));
 		expect(result[0].trust).toEqual(1);
 		expect(result[1].trust).toEqual(2);
 		expect(result[2].trust).toEqual(2);
 		expect(result[3].trust).toEqual(2);
+	});
+	it("should increase trust for only index 0 and 1", () => {
 		data.forEach((x) => {
 			x.trust = 1;
 		});
-	});
-	it("should increase trust for only index 0 and 1", () => {
-		const options: TrustCalcOptions = {
-			notBetween0and10: true,
-			differentNr: true,
-			notMultipleOf10and5: false,
-			notBetween0and100: false,
-			reduceTrust: 0,
-		};
+		options.differentNr = true;
 		const result = calcTrust(data, JSON.stringify(options));
 		expect(result[0].trust).toEqual(2);
 		expect(result[1].trust).toEqual(3);
 		expect(result[2].trust).toEqual(2);
 		expect(result[3].trust).toEqual(2);
+	});
+	it("should increase trust for all but index 2", () => {
 		data.forEach((x) => {
 			x.trust = 1;
 		});
-	});
-	it("should increase trust for all but index 2", () => {
-		const options: TrustCalcOptions = {
-			notBetween0and10: true,
-			differentNr: true,
-			notMultipleOf10and5: true,
-			notBetween0and100: false,
-			reduceTrust: 0,
-		};
+		options.notMultipleOf10and5 = true;
 		const result = calcTrust(data, JSON.stringify(options));
 		expect(result[0].trust).toEqual(3);
 		expect(result[1].trust).toEqual(4);
 		expect(result[2].trust).toEqual(2);
 		expect(result[3].trust).toEqual(3);
+	});
+	it("should increase trust for all but index 0 and 3", () => {
 		data.forEach((x) => {
 			x.trust = 1;
 		});
-	});
-	it("should increase trust for all but index 0 and 3", () => {
-		const options: TrustCalcOptions = {
-			notBetween0and10: true,
-			differentNr: true,
-			notMultipleOf10and5: true,
-			notBetween0and100: true,
-			reduceTrust: 0,
-		};
+		options.notBetween0and100 = true;
 		const result = calcTrust(data, JSON.stringify(options));
 		expect(result[0].trust).toEqual(3);
 		expect(result[1].trust).toEqual(5);
 		expect(result[2].trust).toEqual(3);
 		expect(result[3].trust).toEqual(3);
+	});
+	it("should decrease trust for all by 2", () => {
 		data.forEach((x) => {
 			x.trust = 1;
 		});
-	});
-	it("should decrease trust for all by 2", () => {
-		const options: TrustCalcOptions = {
-			notBetween0and10: true,
-			differentNr: true,
-			notMultipleOf10and5: true,
-			notBetween0and100: true,
-			reduceTrust: 2,
-		};
+		options.reduceTrust = 2;
 		const result = calcTrust(data, JSON.stringify(options));
 		expect(result[0].trust).toEqual(1);
 		expect(result[1].trust).toEqual(3);
 		expect(result[2].trust).toEqual(1);
 		expect(result[3].trust).toEqual(1);
-		data.forEach((x) => {
-			x.trust = 1;
-		});
 	});
 });
