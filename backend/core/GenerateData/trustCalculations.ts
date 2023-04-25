427e753424d5420aece3af10e0690f03201aa1c8
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2023-04-25 11:12:50 +0200

    (refactor) removes trustcalc commented code
    Signed-off-by: Alexander Gemmingen <alexander.gemmingen@gmx.de>

--- backend/core/GenerateData/trustCalculations.ts
+++ backend/core/GenerateData/trustCalculations.ts
@@ -19,51 +19,6 @@ let trustCalcOptions: TrustCalcOptions = {
 	reduceTrust: 1,
 };
 
-/**
- * Adds or removes trust based on the number properties.
- * @param data WorkingData array which to calc trust on
- * @returns
- */
-// export function calcTrust(
-// 	data: WorkingData[],
-// 	trustCalcOptions = getTrustCalcOptions(),
-// ) {
-// 	const options: TrustCalcOptions = JSON.parse(trustCalcOptions);
-// 	data.forEach((x) => {
-// 		Logger.trace(`Before: ${x.date} : ${x.trust}`);
-// 		//if the date is not between 1 and 10, increase trust
-// 		if ((+x.date < 0 || +x.date > 11) && options.notBetween0and10) {
-// 			Logger.trace(`Not between 0 and 10: ${x.date}`);
-// 			x.trust = adjustTrust(x.trust, 1);
-// 		}
-// 		//If many different numbers, more precision, increase trust
-// 		if (new Set([...x.date]).size === x.date.length && options.differentNr) {
-// 			Logger.trace(`Different numbers: ${x.date}`);
-// 			x.trust = adjustTrust(x.trust, 1);
-// 		}
-// 		//if the date is not a multiple of 5, more precision, increase trust
-// 		if (+x.date % 5 !== 0 && options.notMultipleOf10and5) {
-// 			Logger.trace(`Not multiple of 5: ${x.date}`);
-// 			x.trust = adjustTrust(x.trust, 1);
-// 		}
-// 		//If the date is not between 0 and 100 increase trust
-// 		if (+x.date < 0 || (+x.date > 101 && options.notBetween0and100)) {
-// 			Logger.trace(`Not between 0 and 100: ${x.date}`);
-// 			x.trust = adjustTrust(x.trust, 1);
-// 		}
-// 		//reduce trust by one to stop trust inflation
-// 		Logger.debug(`Reduce trust: ${options.reduceTrust}`);
-// 		x.trust = adjustTrust(x.trust, options.reduceTrust * -1);
-// 		return x;
-// 	});
-// 	if (data.length === 1) data[0].trust;
-// 	data.map((x) => {
-// 		Logger.trace(`After: ${x.date} : ${x.trust}`);
-// 		return x;
-// 	});
-// 	return data;
-// }
-
 export function calcTrust(
 	trust: number,
 	date: string,
